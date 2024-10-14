9b4a4d838f7fdfdda37956b9fad0f2e65fb20547
kadu-v <kazuonsunday@yahoo.co.jp>
2024-10-14 14:43:00 +0000

    🚧: WIP to use viuer examples

---
+++ build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // SET Environment variable
+    println!("cargo:rustc-env=TERM_PROGRAM=iTerm.app");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use image::GenericImageView;
+use std::path::Path;
+use std::result::Result;
+use viuer::{print_from_file, Config};
+
 fn main() {
-    println!("Hello, world!");
+    // Load the image from a file using the `image` crate
+    let base_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let img_path = base_path.join("resource").join("test.jpg");
+
+    let mut conf = Config::default();
+    conf.use_kitty = false;
+    conf.use_iterm = true;
+    print_from_file(img_path, &conf).expect("Image printing failed.");
 }
